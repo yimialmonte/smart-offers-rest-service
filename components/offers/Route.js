9247ca64459db70b4a5f8bad9ff7570851d8e694
yimialmonte <yelmialmonte@gmail.com>
2020-11-12 06:51:22 -0400

    create validation for offers

--- components/offers/Route.js
+++ components/offers/Route.js
@@ -1,10 +1,11 @@
 import { Router} from 'express'
 import auth from '../authentication/auth'
 import { createOffer, getOffers } from './Controller';
+import { createOfferValidation } from './Validation';
 
 const offerRoute = new Router();
 
 offerRoute.get('/', getOffers)
-offerRoute.post('/', auth, createOffer)
+offerRoute.post('/', auth, createOfferValidation, createOffer)
 
 export { offerRoute }
